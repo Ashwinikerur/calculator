7b2da1270bf6a286584b561a8e1099700b4fb39e
ASHWINI.KERUR <01fe24bca235@klwtwch.ac.in>
2025-10-17 22:35:41 +0530

    Adding functionalities for mulplication and division

--- calculator.py
+++ calculator.py
@@ -10,6 +10,14 @@ addition = num1 + num2
 # Performing subtraction
 subtraction = num1 - num2
 
+# performing Multiplication
+multiplication = num1 * num2
+
+# performing Division
+division= num1 / num2
+
 # Displaying results
 print("The sum of", num1, "and", num2, "is:", addition)
 print("The difference between", num1, "and", num2, "is:", subtraction)
+print("The product of", num1, "and", num2, "is:", multiplication )
+print("The division of", num1, "and", num2, "is:", division )
